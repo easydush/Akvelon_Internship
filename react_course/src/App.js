72910d1ff6ce44ea6a489e9fa31dd138f995006d
easydush <izidacasillas@mail.ru>
2020-08-05 23:37:25 +0300

    Hooks (1-4)

---
+++ react_course/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LightBulb from "./light";
+import Effect from "./title";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h6>That works!</h6>
+      <LightBulb/>
+      <Effect/>
+    </div>
+  );
+}
+
+export default App;
